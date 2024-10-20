2cc5a104a1256c2f9755a86d93a704a84eb2c15e
Danilo Casabona <danilocasabona>
2024-10-20 20:08:07 -0300

    First Commit

---
+++ primeiraFuncao.py
@@ -0,0 +1,6 @@
+def lambda_handler(event, context):
+    # Mensagem simples para teste
+    return {
+        'statusCode': 200,
+        'body': 'Hello, World! Este é o seu teste de Lambda!'
+    }
