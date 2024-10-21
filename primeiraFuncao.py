d2a5674d0afa7886f42665fe21915f16d46dc716
Danilo Casabona <danilocasabona>
2024-10-20 21:47:05 -0300

    Variavel de ambiente

--- primeiraFuncao.py
+++ primeiraFuncao.py
@@ -2,5 +2,6 @@ def lambda_handler(event, context):
     # Mensagem simples para teste
     return {
         'statusCode': 200,
-        'body': 'Hello, World! Este é o meu teste de Lambda!'
+        'body': 'Hello, World! Este é o meu teste de Lambda!',
+        'ambiente': 'MINHA_VAR'
     }
